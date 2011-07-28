c510dd4a0d90415e2bd0d61b0a13fa68ec5efc41
unknown <meri@.(none)>
2011-07-28 14:20:35 +0200

    First commint: an empty maven eclipse project.

---
+++ src/main/java/org/meri/antlr_step_by_step/App.java
@@ -0,0 +1,13 @@
+package org.meri.antlr_step_by_step;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
